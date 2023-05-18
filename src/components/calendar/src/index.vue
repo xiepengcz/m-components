bc723d7ab717b9329e573cd8f36cddcb6a395ccd
xpc <949026370@qq.com>
2023-05-18 19:46:18 +0800

    优化日历

--- src/components/calendar/src/index.vue
+++ src/components/calendar/src/index.vue
@@ -1,88 +1,6 @@
-<script lang='ts' setup>
-import { defineComponent, ref, reactive } from 'vue'
-import { CalendarOptions, EventApi, DateSelectArg, EventClickArg } from '@fullcalendar/core'
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from './types'
-
-const handleWeekendsToggle = () => {
-  calendarOptions.weekends = !calendarOptions.weekends // update a property
-}
-const handleDateSelect = (selectInfo: DateSelectArg) => {
-  let title = prompt('Please enter a new title for your event')
-  let calendarApi = selectInfo.view.calendar
-
-  calendarApi.unselect() // clear date selection
-
-  if (title) {
-    calendarApi.addEvent({
-      id: createEventId(),
-      title,
-      start: selectInfo.startStr,
-      end: selectInfo.endStr,
-      allDay: selectInfo.allDay
-    })
-  }
-}
-const handleEventClick = (clickInfo: EventClickArg) => {
-  if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-    clickInfo.event.remove()
-  }
-}
-const handleEvents = (events: EventApi[]) => {
-  currentEvents.value = events
-}
-
-const calendarOptions = reactive({
-  plugins: [
-    dayGridPlugin,
-    timeGridPlugin,
-    interactionPlugin // needed for dateClick
-  ],
-  headerToolbar: {
-    left: 'prev,next today',
-    center: 'title',
-    right: 'dayGridMonth,timeGridWeek,timeGridDay'
-  },
-  initialView: 'dayGridMonth',
-  initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
-  editable: true,
-  selectable: true,
-  selectMirror: true,
-  dayMaxEvents: true,
-  weekends: true,
-  select: handleDateSelect,
-  eventClick: handleEventClick,
-  eventsSet: handleEvents
-  /* you can update a remote database when these fire:
-  eventAdd:
-  eventChange:
-  eventRemove:
-  */
-}) as CalendarOptions
-const currentEvents = ref([] as EventApi[])
-
-</script>
-
 <template>
   <div class='demo-app'>
     <div class='demo-app-sidebar'>
-      <div class='demo-app-sidebar-section'>
-        <h2>Instructions</h2>
-        <ul>
-          <li>Select dates and you will be prompted to create a new event</li>
-          <li>Drag, drop, and resize events</li>
-          <li>Click an event to delete it</li>
-        </ul>
-      </div>
-      <div class='demo-app-sidebar-section'>
-        <label>
-          <input type='checkbox' :checked='calendarOptions.weekends' @change='handleWeekendsToggle' />
-          toggle weekends
-        </label>
-      </div>
       <div class='demo-app-sidebar-section'>
         <h2>All Events ({{ currentEvents.length }})</h2>
         <ul>
@@ -103,7 +21,139 @@ const currentEvents = ref([] as EventApi[])
     </div>
   </div>
 </template>
+<script lang='ts' setup>
+import { defineComponent, ref, reactive, PropType } from 'vue'
+import { CalendarOptions, EventApi, DateSelectArg, EventClickArg } from '@fullcalendar/core'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction'
+import { EventItem } from './types'
 
+
+let props = defineProps({
+  // 日历显示的语言
+  local: {
+    type: String,
+    default: 'zh-cn'
+  },
+  // 视图模式
+  initialView: {
+    type: String,
+    default: 'dayGridMonth'
+  },
+  // 按钮文字
+  buttonText: {
+    type: Object,
+    default: () => {
+      return {
+        today: '今天',
+        month: '月',
+        week: '周',
+        day: '日',
+        prevYear: '上一年',
+        nextYear: '下一年',
+        prev: '上一月',
+        next: '下一月'
+      }
+    }
+  },
+  // 头部工具栏
+  headerToolbar: {
+    type: Object,
+    default: () => {
+      return {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      }
+    }
+  },
+  // 底部工具栏
+  footerToolbar: {
+    type: Object
+  },
+  // 日历事件
+  events: {
+    type: Array as PropType<EventItem[]>,
+    default: () => []
+  },
+  // 自定义渲染内容方法
+  eventContent: {
+    type: Function
+  }
+})
+
+
+
+const handleDateSelect = (selectInfo: DateSelectArg) => {
+
+  // let title = prompt('Please enter a new title for your event')
+  // let calendarApi = selectInfo.view.calendar
+
+  // calendarApi.unselect() // clear date selection
+
+  // if (title) {
+  //   calendarApi.addEvent({
+  //     id: new Date().getTime().toString(),
+  //     title,
+  //     start: selectInfo.startStr,
+  //     end: selectInfo.endStr,
+  //     allDay: selectInfo.allDay
+  //   })
+  // }
+}
+const handleEventClick = (clickInfo: EventClickArg) => {
+  console.log('handleEventClick', clickInfo);
+  if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+    clickInfo.event.remove()
+  }
+}
+const handleEvents = (events: EventApi[]) => {
+  currentEvents.value = events
+}
+// 点击日历的某一天
+const dateClick = (info: DateClickArg) => {
+  emits('date-click', info)
+}
+// 点击日历上的时间
+const eventClick = (info: EventClickArg) => {
+  emits('event-click', info)
+}
+const calendarOptions = reactive({
+  plugins: [
+    dayGridPlugin,
+    timeGridPlugin,
+    interactionPlugin // needed for dateClick
+  ],
+  headerToolbar: props.headerToolbar,  // 头部工具栏
+  footerToolbar: props.footerToolbar,// 底部工具栏
+  locale: props.local,
+  buttonText: props.buttonText,
+  initialView: props.initialView,
+  initialEvents: props.events, // alternatively, use the `events` setting to fetch from a feed
+  editable: true,
+  selectable: true,
+  selectMirror: true,
+  dayMaxEvents: true,
+  weekends: true,
+  displayEventEnd: true,// 显示事件的结束时间
+  select: handleDateSelect,
+  eventClick,
+  dateClick,
+  eventsSet: handleEvents,
+
+  /* you can update a remote database when these fire:
+  eventAdd:
+  eventChange:
+  eventRemove:
+  */
+}) as CalendarOptions
+const currentEvents = ref([] as EventApi[])
+
+
+const emits = defineEmits(['date-click', 'event-click'])
+</script>
 <style lang='css'>
 h2 {
   margin: 0;
--- src/views/calendar/index.vue
+++ src/views/calendar/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <m-calendar></m-calendar>
+  <m-calendar :events="events"></m-calendar>
 </template>
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { EventItem } from '../../components/calendar/src/types';
 
+let events = ref<EventItem[]>([
+  {
+    title: '购物',
+    start: '2021-11-11 10:00:00',
+    end: '2021-11-11 12:00:00',
+    editable: true
+  },
+  {
+    title: '学习',
+    start: '2021-11-15 08:00:00',
+    end: '2021-11-15 16:00:00'
+  }
+])
 </script>
-<style lang="scss" scoped>
-  
-</style>
+<style lang="scss" scoped></style>
