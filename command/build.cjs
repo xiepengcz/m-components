9bbad57351015d7c4153067b39ae3bd6eea36749
xpc <949026370@qq.com>
2023-05-20 15:31:56 +0800

    完成按需加载打包

--- command/build.cjs
+++ command/build.cjs
@@ -3,7 +3,9 @@ const path = require('path')
 const { defineConfig, build } = require('vite')
 const vue = require('@vitejs/plugin-vue')
 const vueJsx = require('@vitejs/plugin-vue-jsx')
-
+const fsExtra = require('fs-extra')
+const fs = require('fs')
+const { filter } = require('lodash')
 
 // 打包入口
 const entryDir = path.resolve(__dirname, '../packages')
@@ -44,9 +46,56 @@ const buildAll = async () => {
   })
 }
 
+// 单组件打包构建
+const buildSingle = async (name) => {
+  await build({
+    ...baseConfig,
+    build: {
+      rollupOptions,
+      lib: {
+        entry: path.resolve(entryDir, name),
+        name: 'index',
+        fileName: 'index',
+        formats: ['es', 'umd']
+      },
+      outDir: path.resolve(outDir, name),
+    }
+  })
+}
+// 按需引入时 每个组件要生成package.json
+const createPackageJson = (name) => {
+  const fileStr = `
+  {
+    "name": "${name}",
+    "main" : "index.umd.js", 
+    "module" : "index.js",
+    "style": "style.css"
+  }
+  `
+  // 输出
+  fsExtra.outputFile(
+    path.resolve(outDir, `${name}/package.json`),
+    fileStr,
+    'utf-8'
+  )
+}
+
 
 const buildLib = async () => {
   await buildAll()
+
+  // 获取组件名称组成的数组
+  const components = fs.readdirSync(entryDir).filter(name => { // 读取文件夹
+    const componentDir = path.resolve(entryDir, name)
+    const isDir = fs.lstatSync(componentDir).isDirectory()
+    return isDir && fs.readdirSync(componentDir).includes('index.ts') && name !== 'utils' // 存在目录，并且有index.ts 就是组件
+  })
+
+  // 循环构建
+  for (const name of components) {
+    await buildSingle(name)
+    createPackageJson(name)
+  }
 }
 
 buildLib()
--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,11 @@ import router from "./router";
 import { toLine } from "./utils";
 
 // import chooseArea from "./components/chooseArea";
-import mUI from "../lib/mc-element-components"
+import mUI from "../lib/mc-element-components";
 import '../lib/style.css'
 // import mUI from "./components/index"
-
+// import mChooseIcon from "../lib/chooseIcon";
+// import "../lib/chooseIcon/style.css";
 const app = createApp(App);
 // 全局注册Icon 牺牲一点性能
 // el-icon-xxx
@@ -23,5 +24,6 @@ for (let i in Icons) {
 
 app.use(ElementPlus);
 app.use(mUI);
+// app.use(mChooseIcon)
 app.use(router);
 app.mount("#app");
